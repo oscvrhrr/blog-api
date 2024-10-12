53fde5502ada873f8752bf18f13c532ad6c66ddc
Oscar Herrera <a.oscarherrera@gmail.com>
2024-10-12 11:18:54 -0500

    setup passport

---
+++ auth/passportConfig.js
@@ -0,0 +1,24 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+  
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+        return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+
+
