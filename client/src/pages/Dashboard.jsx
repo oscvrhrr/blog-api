9ecbe1f0a38f30402addc65d4a28cbdf98e7ac50
Oscar Herrera <a.oscarherrera@gmail.com>
2024-10-15 11:10:44 -0500

    created login & dashboard page

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+export default function Dashboard() {
+    return (
+       <main className="text-4xl">
+          This is the Dashboard
+       </main>
+    )
+}
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,46 @@
+// import LoginForm from "../components/LoginForm"
+import LoginForm from "../components/LoginForm"
+import NavBar from "../components/navbar"  
+import RegisterForm from "../components/RegisterForm"
+import { useState } from "react"                       
+
+export default function Landing () {
+    const [isRegisterActive, setIsRegisterActive] = useState(false)
+    const [isLoginActive, setIsLoginActive] = useState(false)
+    
+    const openRegisterForm = () => {
+      if(!isRegisterActive) {
+        setIsRegisterActive(true)
+        setIsLoginActive(false)
+      }
+    }
+
+    const closeRegisterFrom = () => {
+      isRegisterActive? setIsRegisterActive(false): null
+    }
+
+    const openLoginActive = () => {
+     if(!isLoginActive) {
+      setIsLoginActive(true)
+      setIsRegisterActive(false)
+     }
+    }
+
+    const closeLoginActive = () => {
+      isLoginActive? setIsLoginActive(false): null
+    }
+
+
+
+
+    return (
+        <>
+          <main className="bg-[#F7F9FF] h-screen">
+           <NavBar onLoginClick={ openLoginActive } onRegisterClick={openRegisterForm}/>
+           { isRegisterActive && <RegisterForm onToggleClick={ closeRegisterFrom }/> }
+           
+           { isLoginActive && <LoginForm onToggleClick={ closeLoginActive }/>}
+          </main>
+        </>
+    )
+}
