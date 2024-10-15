90c67ae4b0ee0dd8ad98058d9f6973501afbb5dd
Oscar Herrera <a.oscarherrera@gmail.com>
2024-10-15 10:56:47 -0500

    setup jwt strategy

--- auth/passportConfig.js
+++ auth/passportConfig.js
@@ -1,24 +1,56 @@
+// require("dotenv").config();
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const { prisma } = require("../db/queries");
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+}
 
 
 passport.use(
-  new LocalStrategy(async (username, password, done) => {
+  new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
     try {
-      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
-      const user = rows[0];
-  
+      const user = await prisma.user.findFirst({
+        where: { email },
+      })
+
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
       if (user.password !== password) {
         return done(null, false, { message: "Incorrect password" });
       }
+
         return done(null, user);
     } catch(err) {
       return done(err);
     }
   })
 );
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await prisma.user.findFirst({
+        where: { id: jwt_payload.id },
+      });
+
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+
+module.exports = passport
 
 
