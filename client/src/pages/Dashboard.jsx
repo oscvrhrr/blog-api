a71130df1769bc4f173837bd3819e8c246ce33ee
Oscar Herrera <a.oscarherrera@gmail.com>
2024-10-17 13:09:19 -0500

    update, new route & postcontainer

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,34 @@
-
-
+import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom";
+import PostContainer from "../components/PostContainer";
 
 
 export default function Dashboard() {
-    return (
-       <main className="text-4xl">
-          This is the Dashboard
-       </main>
-    )
+   const navigate = useNavigate();
+   
+
+   const handleLogout = () => {
+      localStorage.removeItem("token")
+      navigate("/")
+   }
+
+
+
+
+   return (
+      <main className="bg-[#F7F9FF] h-screen">
+         <nav className="h-12 border-b flex justify-between items-center px-6 text-sm">
+           <div>MyBlog</div>
+           <div className="flex">
+            <Link to='/createpost' className="rounded bg-radixblue-500 inline-block p-1 text-sm mx-4">
+               Create post
+            </Link>
+            <button onClick={ handleLogout } className="rounded bg-radixblue-500 inline-block p-1 text-sm">
+               Logout
+            </button>
+           </div>
+         </nav>
+         <PostContainer/>
+      </main>
+   )
 }
