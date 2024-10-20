f03a225a5e7e16d77a41d8380a7b608324ce3ef6
Oscar Herrera <a.oscarherrera@gmail.com>
2024-10-19 19:32:48 -0500

    wrap user context provider on private routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,22 @@ import { PrivateRoutes } from "./utils/ProtectedRoutes"
 import Landing from "./pages/Landing"
 import Dashboard from "./pages/Dashboard"
 import Createpost from "./pages/Createpost"
-
-
+import { UserContextProvider } from "./components/context/UserContext"
+import PostDetail from "./pages/PostDetail"
 function App() {
 
   return (
     <>
       <Routes>
-        <Route element={ <PrivateRoutes/> }>
-          <Route path="/dashboard"  element={ <Dashboard/> } />
+        <Route element={ 
+            <UserContextProvider>
+              <PrivateRoutes/>
+            </UserContextProvider>
+          }
+        >
+          <Route path="/dashboard"  element={ <Dashboard/> }/>
           <Route path="/createpost" element={ <Createpost/> }/>
+          <Route path="/postdetail/:postId" element={ <PostDetail/>}/>
         </Route>
         <Route path="/" element={ <Landing/> }/>
       </Routes>
