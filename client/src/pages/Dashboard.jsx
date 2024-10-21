b8fafe483d40ce475df4db1296a35b23bead2599
Oscar Herrera <a.oscarherrera@gmail.com>
2024-10-20 19:07:01 -0500

    fetched user data, & set loader

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,17 +1,50 @@
 import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom";
 import PostContainer from "../components/PostContainer";
-
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { UserContext } from "../components/context/UserContext";
 
 export default function Dashboard() {
    const navigate = useNavigate();
-   
+   const { setUser } = useContext(UserContext)
+   const [loading, setLoading] = useState(true);
 
    const handleLogout = () => {
       localStorage.removeItem("token")
       navigate("/")
    }
 
+   useEffect(() => {
+      const fetchUserData = async() => {
+        const token = localStorage.getItem("token")
+        const endpoint = 'http://localhost:4001/user';
+        try {
+            const response = await fetch(endpoint, {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            },
+         })
+         if(response.ok) {
+            const parsedData = await response.json();
+            setUser(parsedData)
+            setLoading(false)
+         }
+        } catch (error) {
+          console.log("error fetching user data", error)
+          setLoading(false)
+        }
+      }
+      fetchUserData()
+   }, [setUser] )
+
+
+   if (loading) {
+      return <div>Loading...</div>
+   }
 
 
 
