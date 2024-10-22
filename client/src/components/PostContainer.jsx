0189844bf7b437225591e3ab40c4a97ef9f617b2
Oscar Herrera <a.oscarherrera@gmail.com>
2024-10-22 11:53:52 -0500

    sorted posts

--- client/src/components/PostContainer.jsx
+++ client/src/components/PostContainer.jsx
@@ -1,14 +1,17 @@
+/* eslint-disable react/prop-types */
 import { Grid  } from "@radix-ui/themes"
 import { useState, useEffect } from "react"
+import { truncateContent, formatDate, sortPosts } from "../lib/helperFunctions"
+import { Link } from "react-router-dom"
 import Post from "./Post"
 
-export default function PostContainer () {
+export default function PostContainer ({ sortCriteria }) {
   const [posts, setPosts] = useState([]);
-
+  
 
   useEffect(() => {
     const fetchPosts = async() => {
-        const endpoint = 'http://localhost:4001/posts';
+        const endpoint = `http://localhost:4001/posts`;
         const token = localStorage.getItem("token");
         try {
          const response = await fetch(endpoint, {
@@ -21,30 +24,30 @@ export default function PostContainer () {
           })
             if(response.ok) {
               const parsedData = await response.json();
-              setPosts(parsedData);
+              const sortedData = sortPosts(parsedData, sortCriteria);
+              setPosts(sortedData);
             }
         } catch(error) {
             console.log(error)
         }
       }
       fetchPosts();
-  }, []);
+  }, [sortCriteria]);
+
   
-  
-
-
-
-
 
     return (
         <>
-          <Grid marg columns="4" gap="4" rows="repeat(1, 280px)" width="auto" className="px-32">
+          <Grid columns="4" gap="4" rows="repeat(1, 350px)" width="auto" className="px-32 pt-12">
             {
                 posts.map((post, index) => (
+                   <Link key={index} to={`/postdetail/${post.id}`}>
                     <Post key={index}
-                      title={post.title}
-                      content={post.content}
+                      title={truncateContent(post.title, 23)}
+                      content={truncateContent(post.content, 100)}
+                      datePosted={formatDate(post.createdAt)}
                     />
+                   </Link>
                 ))
             }
           </Grid>
