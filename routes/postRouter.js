22e34fa9de1994205842dc6d958bd59f36dfd9b4
Oscar Herrera <a.oscarherrera@gmail.com>
2024-10-22 11:06:26 -0500

    protected create post endpoint, & created new comment endpoint

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,6 +1,8 @@
+const passport = require("../auth/passportConfig")
 const Router = require("express");
 
-const { createPost, retrieveAllPosts, retrievePostById } = require("../controllers/postController")
+
+const { createPost, createNewPost, retrieveAllPosts, retrievePostById } = require("../controllers/postController")
 
 const postRouter = Router();
 
@@ -9,11 +11,13 @@ const postRouter = Router();
 
 postRouter.get(("/"), retrieveAllPosts);
 
-postRouter.post(("/"), createPost);
+postRouter.post(("/"), passport.authenticate('jwt', { session: false }), createPost);
 
 postRouter.get(("/:postId"), retrievePostById);
 
 // postRouter.delete(("/:postId"), deletePost);
+
+postRouter.post(("/:postId/comment"), createNewPost)
 
 
 
