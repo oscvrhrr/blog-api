6d91d2336f415dc115ba68d53b6fd91dd0013783
Oscar Herrera <a.oscarherrera@gmail.com>
2024-10-21 19:08:54 -0500

    new icon

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom"
+import { KeyboardIcon } from "@radix-ui/react-icons"
 
 export default function NavBar({ onLoginClick, onRegisterClick }) {
     return (
         <>
            <nav className="h-14 border-b flex justify-between items-center px-6 text-sm relative">
-              <div>
-                MyBlog
+              <div className="flex items-center mx-2">
+                Oscar Writes
+                <KeyboardIcon className="ml-2"/>
               </div>
               <div className="w-36 flex justify-between">
                 <Link onClick={ onLoginClick } className="px-2 py-1 bg-radixblue-500 rounded">Login</Link>
